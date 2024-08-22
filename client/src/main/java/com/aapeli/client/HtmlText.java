0e12b7e59b4e9338d7bdaff2d0405510645dbe3a
Sten Laane <21343173+StenAL@users.noreply.github.com>
2024-08-22 22:07:06 +0300

    Client: Deobfuscate HtmlText and related code
    No functional changes.

--- client/src/main/java/com/aapeli/client/HtmlText.java
+++ client/src/main/java/com/aapeli/client/HtmlText.java
@@ -5,17 +5,17 @@ import java.util.Vector;
 
 public class HtmlText {
 
-    private Vector<Class79> aVector1348;
-    public HtmlText(Graphics var1, int var2, String var3) {
-        this.aVector1348 = this.method1589(var1, var2, var3);
+    private Vector<HtmlLine> lines;
+    public HtmlText(Graphics graphics, int var2, String var3) {
+        this.lines = this.method1589(graphics, var2, var3);
     }
 
     public String toString() {
-        int var1 = this.aVector1348.size();
+        int var1 = this.lines.size();
         String var2 = "[HtmlText: lines.size=" + var1 + "\n";
 
         for (int var3 = 0; var3 < var1; ++var3) {
-            var2 = var2 + " " + this.aVector1348.elementAt(var3).toString() + "\n";
+            var2 = var2 + " " + this.lines.elementAt(var3).toString() + "\n";
         }
 
         var2 = var2 + "]";
@@ -23,61 +23,61 @@ public class HtmlText {
     }
 
     public int print(Graphics var1, int var2, int var3) {
-        int var4 = this.aVector1348.size();
+        int var4 = this.lines.size();
         int var5 = 0;
 
         for (int var7 = 0; var7 < var4; ++var7) {
-            Class79 var6 = this.aVector1348.elementAt(var7);
-            var5 = var6.method1607() + 5;
+            HtmlLine var6 = this.lines.elementAt(var7);
+            var5 = var6.getHeight() + 5;
             if (var7 > 0) {
                 var3 += var5;
             }
 
-            var6.method1608(var1, var2, var3);
+            var6.draw(var1, var2, var3);
         }
 
         return var3 + var5;
     }
 
-    private Vector<Class79> method1589(Graphics var1, int var2, String var3) {
+    private Vector<HtmlLine> method1589(Graphics var1, int var2, String var3) {
         Class81 var4 = new Class81(this, var3, var1);
-        Vector<Class79> var5 = new Vector<>();
+        Vector<HtmlLine> var5 = new Vector<>();
         boolean var6 = false;
-        Class79 var7 = new Class79(this, var1, var2, var6);
+        HtmlLine line = new HtmlLine(this, var1, var2, var6);
 
         String var8;
         while ((var8 = var4.method1611()) != null) {
             if (var8.equals("<br>")) {
-                var5.addElement(var7);
-                var7 = new Class79(this, var1, var2, var6);
+                var5.addElement(line);
+                line = new HtmlLine(this, var1, var2, var6);
             } else if (var8.equals("<center>")) {
                 var6 = true;
-                var7 = this.method1590(var7, var5, var1, var2, var6);
+                line = this.method1590(line, var5, var1, var2, var6);
             } else if (var8.equals("</center>")) {
                 var6 = false;
-                var7 = this.method1590(var7, var5, var1, var2, var6);
+                line = this.method1590(line, var5, var1, var2, var6);
             } else {
                 int var9 = var1.getFontMetrics().stringWidth(var8);
-                if (!var7.method1604(var9)) {
-                    var5.addElement(var7);
-                    var7 = new Class79(this, var1, var2, var6);
+                if (!line.method1604(var9)) {
+                    var5.addElement(line);
+                    line = new HtmlLine(this, var1, var2, var6);
                 }
 
-                var7.method1605(var8, var1.getFont(), var9);
+                line.addWord(var8, var1.getFont(), var9);
             }
         }
 
-        this.method1591(var7, var5);
+        this.method1591(line, var5);
         return var5;
     }
 
-    private Class79 method1590(Class79 var1, Vector<Class79> var2, Graphics var3, int var4, boolean var5) {
+    private HtmlLine method1590(HtmlLine var1, Vector<HtmlLine> var2, Graphics var3, int var4, boolean var5) {
         this.method1591(var1, var2);
-        return new Class79(this, var3, var4, var5);
+        return new HtmlLine(this, var3, var4, var5);
     }
 
-    private void method1591(Class79 var1, Vector<Class79> var2) {
-        if (!var1.method1606()) {
+    private void method1591(HtmlLine var1, Vector<HtmlLine> var2) {
+        if (!var1.isEmpty()) {
             var2.addElement(var1);
         }
 
---
+++ client/src/main/java/com/aapeli/client/HtmlWord.java
@@ -0,0 +1,35 @@
+package com.aapeli.client;
+
+import java.awt.Font;
+import java.awt.Graphics;
+
+class HtmlWord {
+
+    private String word;
+    private Font font;
+    private int start;
+    private int length;
+    private final HtmlLine htmlLine;
+
+
+    protected HtmlWord(HtmlLine htmlLine, String word, Font font, int start, int length) {
+        this.htmlLine = htmlLine;
+        this.word = word;
+        this.font = font;
+        this.start = start;
+        this.length = length;
+    }
+
+    public String toString() {
+        return "[HtmlWord: 'word'=\"" + this.word + "\" 'font'=\"" + this.font.toString() + "\" 'relx'=" + this.start + "]";
+    }
+
+    protected int getLength() {
+        return this.length;
+    }
+
+    protected void draw(Graphics graphics, int x, int y) {
+        graphics.setFont(this.font);
+        graphics.drawString(this.word, x + this.start, y);
+    }
+}
