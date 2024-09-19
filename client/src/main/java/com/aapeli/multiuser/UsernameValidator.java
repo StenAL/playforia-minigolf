c5b1e78e4e921b940a29c84f15c928e7e0cf6456
Sten Laane <21343173+StenAL@users.noreply.github.com>
2024-09-19 18:40:45 +0300

    Client: Allow empty usernames
    These are handled server-side and assigned initialized as an anonymous
    user with names like "anon-xxxx".

--- client/src/main/java/com/aapeli/multiuser/UsernameValidator.java
+++ client/src/main/java/com/aapeli/multiuser/UsernameValidator.java
@@ -3,10 +3,10 @@ package com.aapeli.multiuser;
 import java.util.regex.Pattern;
 
 public class UsernameValidator {
-    public static Pattern INVALID_VALID_USERNAME_PATTERN = Pattern.compile("[^a-zA-Z0-9 ]");
+    public static Pattern INVALID_USERNAME_PATTERN = Pattern.compile("[^a-zA-Z0-9 ]");
 
     public static boolean isValidUsername(String username) {
-        boolean invalid = INVALID_VALID_USERNAME_PATTERN.matcher(username).find() || username.trim().isEmpty();
+        boolean invalid = INVALID_USERNAME_PATTERN.matcher(username).find();
         return !invalid;
 
     }
