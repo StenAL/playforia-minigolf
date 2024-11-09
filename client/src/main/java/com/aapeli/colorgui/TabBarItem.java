3dc976f5b75d10c3892ec7e25d50bb47562ddb6a
Sten Laane <21343173+StenAL@users.noreply.github.com>
2024-11-09 11:52:31 +0200

    Client: Deobfuscate TabBar and TabBarItem code

--- client/src/main/java/com/aapeli/colorgui/TabBarItem.java
+++ client/src/main/java/com/aapeli/colorgui/TabBarItem.java
@@ -5,43 +5,43 @@ import java.awt.Image;
 
 public final class TabBarItem {
 
-    private RadioButton aRadioButton1591;
-    private Component aComponent1592;
-    private int anInt1593;
-    private boolean aBoolean1594;
+    private RadioButton button;
+    private Component component;
+    private int id;
+    private boolean componentAutoSize;
 
-    public TabBarItem(TabBar var1, String var2, Component var3) {
-        this(var1, null, var2, var3);
+    public TabBarItem(TabBar tabBar, String text, Component component) {
+        this(tabBar, null, text, component);
     }
 
-    public TabBarItem(TabBar var1, Image var2, String var3, Component var4) {
-        this.aRadioButton1591 = var1.method876(var2, var3);
-        this.aComponent1592 = var4;
-        this.anInt1593 = 0;
-        this.aBoolean1594 = false;
+    public TabBarItem(TabBar tabBar, Image icon, String text, Component component) {
+        this.button = tabBar.registerItem(icon, text);
+        this.component = component;
+        this.id = 0;
+        this.componentAutoSize = false;
     }
 
-    public void setTabID(int var1) {
-        this.anInt1593 = var1;
+    public void setTabId(int id) {
+        this.id = id;
     }
 
-    public int getTabID() {
-        return this.anInt1593;
+    public int getTabId() {
+        return this.id;
     }
 
-    public void setComponentAutoSize(boolean var1) {
-        this.aBoolean1594 = var1;
+    public void setComponentAutoSize(boolean autoSize) {
+        this.componentAutoSize = autoSize;
     }
 
     public boolean isComponentAutoSize() {
-        return this.aBoolean1594;
+        return this.componentAutoSize;
     }
 
     public RadioButton getButton() {
-        return this.aRadioButton1591;
+        return this.button;
     }
 
     public Component getComponent() {
-        return this.aComponent1592;
+        return this.component;
     }
 }
