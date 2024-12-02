74d62def6caecc14d4f7a26bce5077a426c17feb
Sten Laane <21343173+StenAL@users.noreply.github.com>
2024-12-02 20:39:45 +0200

    Client: Remove UrlLabel dependency on Applet
    Url opening with Desktop.browse()

--- client/src/main/java/com/aapeli/client/UrlLabel.java
+++ client/src/main/java/com/aapeli/client/UrlLabel.java
@@ -1,8 +1,8 @@
 package com.aapeli.client;
 
-import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
@@ -10,8 +10,9 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class UrlLabel extends IPanel implements MouseListener {
 
@@ -24,9 +25,8 @@ public class UrlLabel extends IPanel implements MouseListener {
     private static final Cursor defaultCursor = new Cursor(Cursor.DEFAULT_CURSOR);
     private static final Cursor handCursor = new Cursor(Cursor.HAND_CURSOR);
     private static final Font fontDialog11 = new Font("Dialog", Font.PLAIN, 11);
-    private Applet applet;
     private Font currentFont;
-    private URL url;
+    private URI uri;
     private String text;
     private String linkPlaceholderText;
     private String suffix;
@@ -41,31 +41,20 @@ public class UrlLabel extends IPanel implements MouseListener {
     private int backgroundImageOffsetX;
     private int backgroundImageOffsetY;
     private String urlTarget;
-    private static final String aString3247 = "Dialog";
 
-    public UrlLabel(Applet applet) {
-        this(applet, fontDialog11, null, null);
+    public UrlLabel() {
+        this(fontDialog11, null, null);
     }
 
-    public UrlLabel(Applet applet, String var2, String var3) {
-        this(applet, fontDialog11, var2, var3);
-    }
-
-    public UrlLabel(Applet applet, Font font, String var3, String var4) {
-        this.applet = applet;
+    public UrlLabel(Font font, String text, String url) {
         this.backgroundColor = null;
         this.foregroundMainColor = Color.black;
         this.foregroundSecondaryColor = Color.blue;
         this.backgroundImage = null;
-        this.alignment = -1;
+        this.alignment = ALIGN_LEFT;
         this.urlTarget = "_blank";
         this.currentFont = font;
-        this.setText(var3, var4);
-    }
-
-    public UrlLabel(Applet applet, String var2, String var3, int width, int height) {
-        this(applet, fontDialog11, var2, var3);
-        this.setSize(width, height);
+        this.setText(text, url);
     }
 
     public void update(Graphics graphics) {
@@ -92,64 +81,67 @@ public class UrlLabel extends IPanel implements MouseListener {
         if (this.text != null) {
             int x = 2;
             int fontSize = this.currentFont.getSize();
-            int y = height / 2 + fontSize * 3 / 8;
-            int var8 = height / 2 + fontSize / 2;
+            int textHeight = height / 2 + fontSize * 3 / 8;
+            int y = height / 2 + fontSize / 2;
             if (this.alignment == 0) {
                 x = width / 2 - this.textWidth / 2;
             }
 
-            if (this.alignment == 1) {
+            if (this.alignment == ALIGN_RIGHT) {
                 x = width - 2 - this.textWidth;
             }
 
             if (this.linkPlaceholderText != null && this.suffix != null) {
                 graphics.setColor(this.foregroundSecondaryColor);
-                graphics.drawLine(x + this.prefixWidth, var8, x + this.prefixWidth + this.linkTextWidth, var8);
+                graphics.drawLine(x + this.prefixWidth, y, x + this.prefixWidth + this.linkTextWidth, y);
             }
 
             graphics.setFont(this.currentFont);
             graphics.setColor(this.foregroundMainColor);
-            graphics.drawString(this.text, x, y);
+            graphics.drawString(this.text, x, textHeight);
             if (this.linkPlaceholderText != null && this.suffix != null) {
                 graphics.setColor(this.foregroundSecondaryColor);
-                graphics.drawString(this.linkPlaceholderText, x + this.prefixWidth, y);
+                graphics.drawString(this.linkPlaceholderText, x + this.prefixWidth, textHeight);
                 graphics.setColor(this.foregroundMainColor);
-                graphics.drawString(this.suffix, x + this.prefixWidth + this.linkTextWidth, y);
+                graphics.drawString(this.suffix, x + this.prefixWidth + this.linkTextWidth, textHeight);
             }
         }
     }
 
-    public void mouseEntered(MouseEvent var1) {}
+    public void mouseEntered(MouseEvent e) {}
 
-    public void mouseExited(MouseEvent var1) {}
+    public void mouseExited(MouseEvent e) {}
 
-    public void mousePressed(MouseEvent var1) {}
+    public void mousePressed(MouseEvent e) {}
 
-    public void mouseReleased(MouseEvent var1) {
-        this.applet.getAppletContext().showDocument(this.url, this.urlTarget);
+    public void mouseReleased(MouseEvent e) {
+        try {
+            Desktop.getDesktop().browse(this.uri);
+        } catch (IOException ex) {
+        }
     }
 
-    public void mouseClicked(MouseEvent var1) {}
+    public void mouseClicked(MouseEvent e) {}
 
     public void setText(String text, String url) {
         if (text == null) {
             this.text = this.linkPlaceholderText = this.suffix = null;
-            this.url = null;
+            this.uri = null;
             this.setCursor(defaultCursor);
             this.repaint();
         } else {
-            this.url = null;
+            this.uri = null;
             if (url != null) {
                 try {
-                    this.url = new URL(url);
-                } catch (MalformedURLException e) {
+                    this.uri = new URI(url);
+                } catch (URISyntaxException e) {
                 }
             }
 
-            FontMetrics fontMetrics = this.applet.getFontMetrics(this.currentFont);
+            FontMetrics fontMetrics = this.getFontMetrics(this.currentFont);
             int linkTextStart = text.indexOf('<');
             int linkTextEnd = text.indexOf('>');
-            if (this.url != null && linkTextStart != -1 && linkTextEnd >= linkTextStart) {
+            if (this.uri != null && linkTextStart != -1 && linkTextEnd >= linkTextStart) {
                 this.text = text.substring(0, linkTextStart);
                 this.linkPlaceholderText = text.substring(linkTextStart + 1, linkTextEnd);
                 this.suffix = text.substring(linkTextEnd + 1);
@@ -185,9 +177,9 @@ public class UrlLabel extends IPanel implements MouseListener {
         }
     }
 
-    public void setForeground(Color var1, Color var2) {
-        this.foregroundMainColor = var1;
-        this.foregroundSecondaryColor = var2;
+    public void setForeground(Color mainColor, Color secondaryColor) {
+        this.foregroundMainColor = mainColor;
+        this.foregroundSecondaryColor = secondaryColor;
         this.repaint();
     }
 
@@ -209,73 +201,5 @@ public class UrlLabel extends IPanel implements MouseListener {
 
     public void setTarget(String target) {
         this.urlTarget = target;
-    }
-
-    private static char[] method822(String var0) {
-        char[] var10000 = var0.toCharArray();
-        char[] var10001 = var10000;
-
-        while (true) {
-            int var10002 = var10001.length;
-            var10001 = var10000;
-            int var4 = var10002;
-            if (var10002 >= 2) {
-                break;
-            }
-
-            char[] var1 = var10001;
-            int var2 = var4;
-            var10000 = var1;
-            char[] var10003 = var1;
-            var10002 = var2;
-            var10001 = var10003;
-            if (var10002 != 0) {
-                var10001 = var10000;
-                boolean var3 = false;
-                var10003[0] = (char) (var10003[0] ^ 54);
-                break;
-            }
-        }
-
-        return var10001;
-    }
-
-    private static String method823(char[] var0) {
-        int var10000 = var0.length;
-        int var1 = 0;
-        char[] var10001 = var0;
-        if (var10000 > 1) {
-            var10001 = var0;
-            if (var10000 <= var1) {
-                return (new String(var0)).intern();
-            }
-        }
-
-        do {
-            char[] var10002 = var10001;
-            int var10003 = var1;
-
-            while (true) {
-                char var10004 = var10002[var10003];
-                byte var10005 =
-                        switch (var1 % 5) {
-                            case 0 -> 41;
-                            case 1 -> 61;
-                            case 2, 3 -> 79;
-                            default -> 54;
-                        };
-
-                var10002[var10003] = (char) (var10004 ^ var10005);
-                ++var1;
-                if (var10000 != 0) {
-                    break;
-                }
-
-                var10003 = var10000;
-                var10002 = var10001;
-            }
-        } while (var10000 > var1);
-
-        return (new String(var10001)).intern();
     }
 }
